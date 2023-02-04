bb7d968a84eee7ee1dbf8669289c38d8f65ef4c8
HomoFreak <egementkaraok@gmail.com>
2023-02-04 05:04:15 +0300

    Update CharacterController.cs

--- MiniJameGam#14/Assets/Scripts/CharacterController.cs
+++ MiniJameGam#14/Assets/Scripts/CharacterController.cs
@@ -6,33 +6,27 @@ using UnityEngine.InputSystem;
 public class CharacterController : MonoBehaviour
 {
     public InputAction playerControls;
-    public InputAction playerInteract;
-    public float interactWith;
     Vector2 movedirection = Vector2.zero;
     public float speed = 5f;
+    public float powerupSpeed = 10f;
 
-     void Update()
+    void Update()
     {
         movedirection = playerControls.ReadValue<Vector2>();
 
         transform.position += transform.forward * movedirection.y * speed * Time.deltaTime;
 
         transform.position += transform.right * movedirection.x * speed * Time.deltaTime;
-
-        interactWith = playerInteract.ReadValue<float>();
-        Debug.Log("Interact value: " + interactWith);
     }
 
     private void OnEnable()
     {
         playerControls.Enable();
-        playerInteract.Enable();
     }
 
     private void OnDisable()
     {
         playerControls.Disable();
-        playerInteract.Disable();
     }
 }
     
